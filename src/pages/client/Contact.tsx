467fa6086e0400b3f51ce9c392f93448e2855695
brolol <anshajshaji3@gmail.com>
2026-03-06 20:53:04 +0530

    Testing with email12312312

--- src/pages/client/Contact.tsx
+++ src/pages/client/Contact.tsx
@@ -27,7 +27,7 @@ const Contact: React.FC = () => {
     const data = Object.fromEntries(formData.entries());
 
     try {
-      const response = await fetch("https://formsubmit.co/ajax/plggdinn@gmail.com", {
+      const response = await fetch("https://formsubmit.co/ajax/loveheartstvm@gmail.com", {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
